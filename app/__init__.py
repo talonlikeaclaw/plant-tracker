4717cf5be6d296db2ed0f7eeff6e5cdd9d3b1872
talonlikeaclaw <talonlikeaclaw@proton.me>
2025-05-18 23:03:20 -0400

    feat(init): add static folder and url path to app creation

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 def create_app():
     """Creates the Flask application"""
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder="static", static_url_path="/static")
     app.config.from_object("config.Config")
 
     return app
