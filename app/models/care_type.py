d2c6e03bed62305988d92d1de737759aa14c18f2
talonlikeaclaw <talonlikeaclaw@proton.me>
2025-06-05 16:05:36 -0400

    refactor: convert care_type model into sqlalchemy orm model

--- app/models/care_type.py
+++ app/models/care_type.py
@@ -1,9 +1,17 @@
-class CareType:
-    """Represents a Type of Plant Care"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from app.models.database import Base
 
-    def __init__(self, **kwargs):
-        """Initializes a global/user Care Type"""
-        self.id = kwargs.get("care_type_id")
-        self.user_id = kwargs.get("user_id")
-        self.name = kwargs.get("name")
-        self.description = kwargs.get("description")
+
+class CareType(Base):
+    """Represents a Type of Plant Care"""
+    __tablename__ = "care_types"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    name = Column(String, nullable=False)
+    description = Column(String)
+
+    user = relationship("User", back_populates="care_types")
+    plant_care_logs = relationship(
+        "PlantCare", back_populates="care_type", cascade="all, delete")
