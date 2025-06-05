73c82fe7b22a55bdd401d4d00574244ef0cbf9e3
talonlikeaclaw <talonlikeaclaw@proton.me>
2025-06-05 17:38:40 -0400

    feat: import and register model classes

---
+++ app/models/__init__.py
@@ -0,0 +1,5 @@
+from app.models.care_type import CareType
+from app.models.plant import Plant
+from app.models.plant_care import PlantCare
+from app.models.species import Species
+from app.models.user import User
