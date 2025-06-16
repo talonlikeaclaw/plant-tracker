18e97fdc767a4a17aa2fabe4ad553ad251a7e20e
talonlikeaclaw <talonlikeaclaw@proton.me>
2025-06-16 16:21:47 -0400

    feat: implement api route for updating a user's password

--- app/api/users.py
+++ app/api/users.py
@@ -102,3 +102,52 @@ def update_user(user_id):
 
     finally:
         db.close()
+
+
+@user_bp.route("/<int:user_id>/password", methods=["PATCH"])
+@jwt_required()
+def update_password(user_id):
+    """Updates a User's password.
+
+    Args:
+        user_id (int): The ID of the User to update.
+    """
+    db = SessionLocal()
+    user_service = UserService(db)
+    auth_service = AuthService(user_service)
+
+    current_user_id = int(get_jwt_identity())
+    if current_user_id != user_id:
+        return jsonify({"error": "Unauthorized"}), 403
+
+    try:
+        data = request.get_json()
+
+        email = data.get("email")
+        old_password = data.get("old_password")
+        new_password = data.get("new_password")
+        confirm_password = data.get("confirm_password")
+
+        if not email or not old_password or not new_password or not confirm_password:
+            return jsonify({"error": "All fields are required"}), 400
+
+        if new_password != confirm_password:
+            return jsonify(
+                {"error": "The new_password and confirm_passwond fields must match."}
+            ), 400
+
+        user = auth_service.authenticate_user(email, old_password)
+        if not user:
+            return jsonify({"error": "Invalid credentials"}), 401
+
+        hashed_password = auth_service.hash_password(new_password)
+        user_service.update_user(user_id, {"password_hash": hashed_password})
+
+        return jsonify({"message": "Password updated successfully!"}), 200
+
+    except Exception as e:
+        db.rollback()
+        return jsonify({"error": str(e)}), 400
+
+    finally:
+        db.close()
