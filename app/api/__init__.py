2f201c51e5828cd57013f92de4728fb637611ebf
talonlikeaclaw <talonlikeaclaw@proton.me>
2025-07-05 22:26:43 -0400

    feat: register species blueprint and initialize

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,9 +1,11 @@
 from .auth import auth_bp
 from .users import user_bp
 from .plants import plant_bp
+from .species import species_bp
 
 
 def register_api_blueprints(app):
     app.register_blueprint(auth_bp, url_prefix="/api/auth")
     app.register_blueprint(user_bp, url_prefix="/api/users")
     app.register_blueprint(plant_bp, url_prefix="/api/plants")
+    app.register_blueprint(species_bp, url_prefix="/api/species")
