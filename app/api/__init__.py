0e330cb40e66e419d1d9fe5e0fe1313aa807b256
talonlikeaclaw <talonlikeaclaw@proton.me>
2025-07-07 17:21:58 -0400

    feat: register care_type_bp blueprint and fix plant_care_bp registration

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -4,6 +4,7 @@ from .users import user_bp
 from .plants import plant_bp
 from .species import species_bp
 from .plant_care import plant_care_bp
+from .care_type import care_type_bp
 
 
 def register_api_blueprints(app):
@@ -11,4 +12,5 @@ def register_api_blueprints(app):
     app.register_blueprint(user_bp, url_prefix="/api/users")
     app.register_blueprint(plant_bp, url_prefix="/api/plants")
     app.register_blueprint(species_bp, url_prefix="/api/species")
-    app.register_blueprint(plant_care_service, url_prefix="/api/plant_care")
+    app.register_blueprint(plant_care_bp, url_prefix="/api/plant_care")
+    app.register_blueprint(care_type_bp, url_prefix="/api/care_types")
