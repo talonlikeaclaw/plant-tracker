b22c9ff5689a54838fe085419ec94503688e4e0e
talonlikeaclaw <talonlikeaclaw@proton.me>
2025-07-06 20:00:51 -0400

    feat: register plant care log blueprint and initialize

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,7 +1,9 @@
+from app.services import plant_care_service
 from .auth import auth_bp
 from .users import user_bp
 from .plants import plant_bp
 from .species import species_bp
+from .plant_care import plant_care_bp
 
 
 def register_api_blueprints(app):
@@ -9,3 +11,4 @@ def register_api_blueprints(app):
     app.register_blueprint(user_bp, url_prefix="/api/users")
     app.register_blueprint(plant_bp, url_prefix="/api/plants")
     app.register_blueprint(species_bp, url_prefix="/api/species")
+    app.register_blueprint(plant_care_service, url_prefix="/api/plant_care")
