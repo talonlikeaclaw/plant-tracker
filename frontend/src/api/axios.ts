db40a7e7453c5634189eee7d7e73ae6f23ca7de4
talonlikeaclaw <talonlikeaclaw@proton.me>
2025-07-28 14:33:46 -0400

    create interceptor that automatically adds auth token headers to requests

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -7,4 +7,17 @@ const api = axios.create({
   },
 });
 
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers = {
+      ...config.headers,
+      Authorization: `Bearer ${token}`,
+    };
+  }
+
+  return config;
+});
+
 export default api;
