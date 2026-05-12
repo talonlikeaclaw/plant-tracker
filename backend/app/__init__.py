ffe5b1843ee58f696373d6976724ebfa2628ac9d
talonlikeaclaw <talonlikeaclaw@proton.me>
2026-05-11 21:40:40 -0400

    Update init and nginx.conf for cloudflare tunnel

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -13,7 +13,7 @@ def create_app():
     CORS(
         app,
         supports_credentials=True,
-        origins=["http://localhost:5173", "http://localhost:3000"],
+        origins=["http://localhost:5173", "http://localhost:3000", "https://plants.talonlikeaclaw.com"],
         allow_headers=["Content-Type", "Authorization"],
         methods=["GET", "POST", "PATCH", "DELETE", "OPTIONS"]
     )
